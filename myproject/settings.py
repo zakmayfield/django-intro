f4ec7c3c1219263c2d23e28c31d9c89429edfe7f
Zak Mayfield <zakmayfield@gmail.com>
2023-07-24 15:48:45 -0400

    migrations

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,8 @@ DATABASES = {
    "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "first-django-app",
-        "USER": "zakmayfield",
-        "PASSWORD": "mellon",
+        "USER": "user",
+        "PASSWORD": "password",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
